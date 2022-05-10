6c138c1bd2bb4bebc22874a435f9504305446fa8
emonmorol <ekramulhasane69@gmail.com>
2022-05-11 02:56:48 +0600

    database connected with mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctorsportal.oafat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  console.log("db connected");
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("Rolling From Doctors Portal");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Rolling Portal from`, port);
 });
